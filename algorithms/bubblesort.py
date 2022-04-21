de5a0e34ee7d86cf793ae9d83ccb4d83ddb578d8
Gabriele Gorini <gabriele.gorini95@gmail.com>
2022-04-21 12:23:43 +0200

    Optimized BubbleSort

--- algorithms/bubblesort.py
+++ algorithms/bubblesort.py
@@ -4,11 +4,16 @@ from colors import *
 
 def bubble_sort(data, drawData, timeTick):
     size = len(data)
-    for i in range(size - 1):
-        for j in range(size - i - 1):
-            if data[j] > data[j+1]:
-                data[j], data[j+1] = data[j+1], data[j]
+    for i in range(size):
+        swapped = False
+        for j in range(0, size-i-1):
+            if data[j] > data[j+1]:   
                 drawData(data, [YELLOW if x == j or x == j+1 else BLUE for x in range(len(data))])
+                data[j], data[j+1] = data[j+1], data[j]
+                swapped = True
                 time.sleep(timeTick)
+        
+        if swapped == False:
+            break
     
     drawData(data, [BLUE for x in range(len(data))])
