5221c5fd472cf0f7cd1f0947bdff5dae1008d6a6
Gabriele Gorini <gabriele.gorini95@gmail.com>
2022-04-21 11:23:21 +0200

    Implemented generating random array and drawing bars

--- main.py
+++ main.py
@@ -8,13 +8,38 @@ from colors import BLACK
 #Creating the window
 window = Tk()
 window.title("Sorting Algorithm Visualizer")
-window.geometry("%sx%s" % (window.winfo_screenwidth(), window.winfo_screenheight()))
-window.maxsize(window.winfo_screenwidth(), window.winfo_screenheight())
-window.config(bg = 'WHITE')
+window.maxsize(1000, 700)
+window.config(bg = 'white')
+
+#This function will draw the randomly generated data on the canvas as vertical bars
+def drawdata(data, colorArray):
+    canvas.delete("all")
+    canvas_width = 800
+    canvas_height = 400
+    x_width = canvas_width / (len(data) + 1)
+    offset = 4
+    spacing = 2
+    normalizedData = [i / max(data) for i in data]
+
+    for i, height in enumerate(normalizedData):
+        x0 = i * x_width + offset + spacing
+        y0 = canvas_height - height * 390
+        x1 = (i + 1) * x_width + offset
+        y1 = canvas_height
+        canvas.create_rectangle(x0, y0, x1, y1, fill=colorArray[i])
+
+    window.update_idletasks()
 
 #This function will generate the array with random values
 def generate():
-    pass
+    global data
+
+    data = []
+    for i in range(0, 100):
+        random_value = random.randint(1, 150)
+        data.append(random_value)
+
+    drawdata(data, ['BLUE' for x in range(len(data))])
 
 #This function will start the sorting process with the selected algorithm
 def sort():
@@ -32,7 +57,7 @@ speed_list = ['Fast', 'Medium', 'Slow']
 data = []
 
 #UI generation
-UI_Frame = Frame(window, width=window.winfo_width(), height=300, bg='black')
+UI_Frame = Frame(window, width=900, height=300, bg='white')
 UI_Frame.grid(row=0, column=0, padx=10, pady=5)
 
 #Algorithm selection dropdown menu
@@ -58,7 +83,7 @@ generate_btn = Button(UI_Frame, text = "GenerateArray", command=generate, bg='gr
 generate_btn.grid(row=2, column=0, padx=5, pady=5)
 
 #Canvas for array drawing
-canvas = Canvas(window, width=800, height=400, bg='black')
+canvas = Canvas(window, width=800, height=400, bg='white')
 canvas.grid(row=1, column=0, padx=10, pady=5)
 
 window.mainloop()
